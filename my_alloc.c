0e95ff0cb3a97dda663cbdf68bd1218676f8365f
Emma Christy <emma.t.christy@gmail.com>
2025-03-14 14:33:23 +0200

    added functions for manipulating block headers

--- my_alloc.c
+++ my_alloc.c
@@ -4,7 +4,7 @@
  *
  * the lower 4 bits of any address and size will be 0 because of the the 16 alignment
  * header layout:
- * 0	allocated status
+ * 0	set if block is allocated
  * 4-63	upper bits of size in bytes
  *
  * 0	set if block is at the end of the list
@@ -13,13 +13,68 @@
  * size classes are separated based on the leftmost 1 in the binary representation of size
 */
 #include <stdlib.h>
+#include <stdint.h>
 
 static void *heap_base_ptr = NULL;//points to the base address of the heap
 static size_t heap_size = 0;//size of heap in bytes
 static void *heap_end_ptr = NULL;//points to the end of the heap this address itself is not writable
 
-#define INI_HEAP_SIZE = 1024//in bytes
-
-#define HEADER_SIZE = 16//in bytes
+#define INI_HEAP_SIZE 1024//in bytes
+#define HEADER_SIZE 16//in bytes
 
 static void *size_class_arr[60];
+
+typedef struct{
+	size_t size;
+	void *next_adr;
+}header;
+
+static size_t get_block_size(header *header_ptr)
+{
+	return header_ptr->size & (~0xf);
+}
+
+static int is_allocated(header *header_ptr)
+{
+	return header_ptr->size & 0x1;
+}
+
+static void *get_next_block_ptr(header *header_ptr)
+{
+	return ((void *) (((uintptr_t) header_ptr->next_adr) & (~0xf)));
+}
+
+static int is_list_end(header *header_ptr)
+{
+	return (uintptr_t) header_ptr->next_adr & 0x1;
+}
+
+static void set_block_allocated(header *header_ptr)
+{
+	header_ptr->size |= 0x1;
+}
+
+static void set_block_free(header *header_ptr)
+{
+	header_ptr->size &= ~0x1;
+}
+
+static void set_block_list_end(header *header_ptr)
+{
+	header_ptr->next_adr = (void *) ((uintptr_t) header_ptr->next_adr | 0x1);
+}
+
+static void unset_block_list_end(header *header_ptr)
+{
+	header_ptr->next_adr = (void *) ((uintptr_t) header_ptr->next_adr & ~0x1);
+}
+
+static void set_block_size(header *header_ptr, size_t size)
+{
+	header_ptr->size = size & (~0xf);
+}
+
+static void set_next_block_ptr(header *header_ptr, void *next_block_ptr)
+{
+	header_ptr->next_adr = (void *) (((uintptr_t) next_block_ptr) & (~0xf));
+}
