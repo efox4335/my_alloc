bd2bd451beb2d510b791134517b3dd97c73584da
Emma Christy <emma.t.christy@gmail.com>
2025-04-04 14:01:13 +0300

    added test to check if writing to allocated buffer will cause a seg fault

---
+++ tests/seg_fault.c
@@ -0,0 +1,22 @@
+/*
+ * tests weather a seg fault will happen if buffer is written to
+*/
+
+#include "my_alloc.h"
+#include "utils.c"
+
+#define BUF_SIZE 1000
+#define SEED 2342235
+
+int main(void)
+{
+	void *ptr = my_alloc(BUF_SIZE);
+
+	set_mem(ptr, SEED, BUF_SIZE);
+
+	int ret_val = 0;
+
+	my_free(ptr);
+
+	return ret_val;
+}
